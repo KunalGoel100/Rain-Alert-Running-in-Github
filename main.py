76e103c106e9c1720facff2574bc1ac0dadf1a9e
KunalGoel100 <kunal2000goel@gmail.com>
2026-02-20 20:27:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+ApiKey = os.environ.get("ApiKey")
+address = "https://api.openweathermap.org/data/2.5/forecast"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+param = {"lat":28.704060,
+         "lon":77.102493,
+         "cnt":1,
+         "appid":"8f3464385339a75ee7d59e18b8e6e7bb"
+         }
+Forecast = ""
+response = requests.get(url=address,params=param)
+data = response.json()["list"]
+day = data[0]
+if day["weather"][0]["id"] < 300:
+    Forecast = "ThunderStormy"
+elif day["weather"][0]["id"] < 400:
+    Forecast = "Drizzly"
+elif day["weather"][0]["id"] < 600:
+    Forecast = "Rainy"
+elif day["weather"][0]["id"] < 700:
+    Forecast = "Snowy"
+elif day["weather"][0]["id"] >= 800:
+    Forecast = "Cloudy"
+else:
+    pass
+print(Forecast)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+Actual_SID = os.environ.get("Actual_SID")
+Actual_Token = os.environ.get("Actual_Token")
+Actual_Number = os.environ.get("Actual_Number")
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+client = Client(Actual_SID,Actual_Token)
+message = client.messages.create(
+    body = f"Hi there, The weather forecast by Kunal, for you is, {Forecast}! ",
+    from_ = Actual_Number,
+    to = "whatsapp:+919654291639"
+)
+print(message.status)
